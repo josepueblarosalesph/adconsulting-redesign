0554944fc76c98703335a394b3df768573339fd3
josepueblaphcs <jose.puebla@phcs.cl>
2026-09-09 21:04:28 -0300

    Rename AD Black and simplify external menu links

--- app/navigation.ts
+++ app/navigation.ts
@@ -1,1 +1,1 @@
-export const links=[['Inicio','/'],['Nosotros','/nosotros/'],['Servicios','/servicios/'],['Noticias','/noticias/'],['AD+50','/ad50/'],['ADblack','/black/']];
+export const links=[['Inicio','/'],['Nosotros','/nosotros/'],['Servicios','/servicios/'],['Noticias','/noticias/'],['AD+50','/ad50/'],['AD Black','/black/']];
